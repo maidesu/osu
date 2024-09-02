f8ff3860d5cfccff275d9bde33aad531aa14e8b5
maidesu <vorpeti@gmail.com>
2024-09-02 13:52:43 +0200

    Add slider indicator mod

--- osu.Game.Rulesets.Osu/OsuRuleset.cs
+++ osu.Game.Rulesets.Osu/OsuRuleset.cs
@@ -212,7 +212,8 @@ namespace osu.Game.Rulesets.Osu
                         new OsuModFreezeFrame(),
                         new OsuModBubbles(),
                         new OsuModSynesthesia(),
-                        new OsuModDepth()
+                        new OsuModDepth(),
+                        new OsuModSliderIndicator()
                     };
 
                 case ModType.System:
